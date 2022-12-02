33d5361ab79c3a298a69a9833083e099d2b4d7ce
Yudota <yuri.fernandes.961@gmail.com>
2022-12-02 12:30:39 -0300

    feat: create facade scaffolding

---
+++ src/facades/IFacade.ts
@@ -0,0 +1,6 @@
+import { TYPE_COMMAND } from "../commands/Command";
+import AbsEntidadeDominio from "../models/AbsEntidadeDominio";
+
+export default interface IFacade {
+    processar: (command: TYPE_COMMAND, entidade: AbsEntidadeDominio) => AbsEntidadeDominio
+}
