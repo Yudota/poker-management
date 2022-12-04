914b08fcc76c5d4a60d91fba14d5ceb4b082aa6c
devkemc <joaquimkbs@gmail.com>
2022-12-04 00:02:40 -0300

    refactor: deixando controller funcional

--- src/apresentation/controller/Controller.ts
+++ src/apresentation/controller/Controller.ts
@@ -1,19 +1,37 @@
 import { Request, Response } from "express";
 import { ICommand } from "../../commands/ICommand";
 import { IViewHelper } from "../vh/IViewHelper";
+import { MethodRequestTypes } from "./RequesType";
+import { ViewHelperPlayer } from "../vh/ViewHelperPlayer";
+import { ListAllCommand } from "../../commands/ListAllCommand";
 
 export class Controller {
   private _url: string;
   private _operacao: string;
-  private commands = Map<string, ICommand>;
-  private viewHelper = Map<string, IViewHelper>;
+  private commands: Map<String, ICommand>;
+  private viewHelpers: Map<String, IViewHelper>;
+  private vh: IViewHelper | undefined = undefined;
+  private cmd: ICommand | undefined = undefined;
   constructor() {
     this._url = "";
     this._operacao = "";
+
+    this.handle = this.handle.bind(this);
+    this.commands = new Map<String, ICommand>();
+    this.commands.set(MethodRequestTypes.GET, new ListAllCommand());
+
+    this.viewHelpers = new Map<String, IViewHelper>();
+    this.viewHelpers.set("/players", new ViewHelperPlayer());
   }
-  async doGet(req: Request, resp: Response) {
+  async handle(req: Request, resp: Response) {
     this._url = req.url;
     this._operacao = req.method;
-    console.log(req.method);
+
+    this.vh = this.viewHelpers.get(this._url);
+    const entidade = this.vh?.getEntidade(req);
+
+    this.cmd = this.commands.get(this._operacao);
+
+    this.cmd?.executar(entidade);
   }
 }
