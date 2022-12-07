4361440fa307535e117f9172d506a3b3cf07f1a9
Yudota <yuri.fernandes.961@gmail.com>
2022-12-07 04:45:00 -0300

    wip: creating JogadorDAO

--- src/DAO/JogadorDAO.ts
+++ src/DAO/JogadorDAO.ts
@@ -1,14 +1,28 @@
-import AbsEntidadeDominio from "../models/AbsEntidadeDominio";
-import Jogador from "../models/Jogador";
+import ConnectionFactory from "./ConnectionFactory";
 import IDAO from "./IDAO";
 
+import Jogador from "../models/Jogador";
+import Result from "../utils/Result";
+
 export default class JogadorDAO implements IDAO {
+    con: any;
+    constructor() {
+        this.con = ConnectionFactory.criar()
+    }
+    criar(entidade: Jogador): Promise<Result> {
+        throw new Error("Method not implemented.");
+    }
+    alterar(entidade: Jogador): Promise<Result> {
+        throw new Error("Method not implemented.");
+    }
+    excluir(id: number): Promise<Result> {
+        throw new Error("Method not implemented.");
+    }
+    consultar(entidade: Jogador): Promise<Result> {
+        throw new Error("Method not implemented.");
+    }
     getAll(): Array<Jogador> {
         return []
     };
-    getByID!: (id: number) => AbsEntidadeDominio;
-    save!: (entity: AbsEntidadeDominio) => Promise<void>;
-    update!: (entity: AbsEntidadeDominio) => Promise<AbsEntidadeDominio>;
-    remove!: (entity: AbsEntidadeDominio) => Promise<void>;
 
 }
