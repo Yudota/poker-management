675c0486b96aa0214bf06f345c8b2a239a2f0406
Yudota <yuri.fernandes.961@gmail.com>
2022-12-07 04:44:13 -0300

    refactor: set Response as default return on abstract class

--- src/commands/AbsCommand.ts
+++ src/commands/AbsCommand.ts
@@ -2,10 +2,11 @@ import Facade from "../facades/Facade";
 import IFacade from "../facades/IFacade";
 import { ICommand } from "./ICommand";
 import AbsEntidadeDominio from "../models/AbsEntidadeDominio";
+import Result from "../utils/Result";
 
 export abstract class AbsCommand implements ICommand {
-  executar(entidade: AbsEntidadeDominio) {
-    return "";
+  async executar(entidade: AbsEntidadeDominio) {
+    return new Result('');
   }
   protected facade: IFacade = new Facade();
 }
