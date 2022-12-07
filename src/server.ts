0ae0e1badbadecbe896b2059872ef362793b5ff2
Yudota <yuri.fernandes.961@gmail.com>
2022-12-07 15:13:31 -0300

    feat: implement cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { routes } from "./routes";
+import cors from 'cors';
 
 const app = express();
+
+
+app.use(cors());
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.json());
 
 app.use(routes);
