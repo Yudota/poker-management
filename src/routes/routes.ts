c66ad60abf1546b7328d40fcbc08e352f51a2d32
Yudota <yuri.fernandes.961@gmail.com>
2022-12-12 07:44:35 -0300

    refactor: implement loop for construct routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { Controller } from "../presentation/controller/Controller";
 
+enum endpoints {
+    PLAYER = "player",
+    CIDADE = "cidade",
+    ESTADO = "estado"
+}
 
 const routes = Router();
-const endpoint = "player";
 const controller = new Controller();
 
-routes.get(`/${endpoint}`, controller.handle);
-routes.post(`/${endpoint}`, controller.handle);
-routes.put(`/${endpoint}`, controller.handle);
-routes.delete(`/${endpoint}`, controller.handle);
+for (const endpoint in endpoints) {
+    routes.get(`/${endpoint}`, controller.handle);
+    routes.post(`/${endpoint}`, controller.handle);
+    routes.put(`/${endpoint}`, controller.handle);
+    routes.delete(`/${endpoint}`, controller.handle);
+}
 export { routes };
